26654755534c59ca3dcd0a3fb8d79cd6c9bddc02
domberardi10 <domberardi10@gmail.com>
2022-05-19 18:03:04 -0700

    Began to plan out levels in Tiled
    Using a temp tileset of solid colors. First version of Level 1 tilemap set up.

---
+++ assets/test_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="test_tileset" tilewidth="64" tileheight="64" tilecount="12" columns="4">
+ <image source="test_tileset.png" width="256" height="192"/>
+</tileset>
